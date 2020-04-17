9b9e82d557a68ca9951d105f5ec4ae932bd82d9e
srwan <baizq2019@163.com>
2020-04-17 20:01:51 +0800

    mybatis配置

---
+++ src/main/java/com/srwan/mall/config/MyBatisConfig.java
@@ -0,0 +1,14 @@
+package com.srwan.mall.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatis配置类
+ *
+ */
+
+@Configuration
+@MapperScan({"com.srwan.mall.mbg.mapper","com.srwan.mall.dao"})
+public class MyBatisConfig {
+}
